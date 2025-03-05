bcd458fa29277d576f913a6e2eacc32a77522572
dmutro <shaduraa.dmutroo@gmail.com>
2025-03-05 02:57:07 +0200

    create simple cli

--- src/main.c
+++ src/main.c
@@ -3,13 +3,6 @@
 
 #include "blueis.h"
 
-void add_to_table(BlueisTable *table) {
-  BlueisValue key = TO_BLUEIS_VALUE("Hello");
-  BlueisValue value = TO_BLUEIS_VALUE("World");
-
-  blueis_table_insert(table, key, value);
-}
-
 void print_blueis_value(BlueisValue value) {
   switch (value.kind) {
     case BLUEIS_VALUE_NUMBER:
@@ -30,15 +23,21 @@ int main() {
   BlueisTable table;
   blueis_table_init(&table);
 
-  add_to_table(&table);
+  char buf[1024];
+  while (1) {
+    printf("> ");
+    if (!fgets(buf, sizeof(buf), stdin)) {
+      break;
+    }
 
-  BlueisValue get_res = blueis_execute_cmd(&table, "GET \"Hello\"");
-  BlueisValue set_res = blueis_execute_cmd(&table, "SET \"Hello\" \"it works\"");
-  
-  print_blueis_value(get_res);
-  printf("\n");
-  print_blueis_value(set_res);
-  printf("\n");
+    if (strcmp(buf, "exit\n") == 0) {
+      break;
+    }
+
+    BlueisValue res = blueis_execute_cmd(&table, buf);
+    print_blueis_value(res);
+    printf("\n");
+  }
 
   blueis_table_deinit(&table);
   return 0;
